e01f3d05451a0b81843ecf8dc924da61710c1e38
sgomezp <sgomezp@gmail.com>
2018-03-21 20:58:16 +0100

    Using buildConfig property -- API_KEY

--- app/src/main/java/com/example/android/popularmoviesV1/utils/Constants.java
+++ app/src/main/java/com/example/android/popularmoviesV1/utils/Constants.java
@@ -1,5 +1,7 @@
 package com.example.android.popularmoviesV1.utils;
 
+import com.example.android.popularmoviesV1.BuildConfig;
+
 /**
  * Created by sgomezp on 05/03/2018.
  */
@@ -8,7 +10,7 @@ public class Constants {
     public static final String BASE_URL = "http://api.themoviedb.org/3/";
     public static final String URL_POPULAR_MOVIES = "movie/popular";
     public static final String URL_TOP_RATED = "movie/top_rated";
-    public static final String API_KEY = "";
+    public static final String API_KEY = BuildConfig.API_KEY;
     public static final String URL_IMAGE = "http://image.tmdb.org/t/p/w185";
 
 
